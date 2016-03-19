1b33458633d0726a6c98f23b40ba8e1c3ea4d574
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-03-19 17:02:11 +0000

    MusucData.csv and MusicDataNorm.csv created and added. R file modified.

--- MusicDataSetNormalisation.R
+++ MusicDataSetNormalisation.R
@@ -2,3 +2,18 @@ install.packages("dprep")
 library(dprep)
 
 musicDataSet <- read.csv("MusicDataSet.csv", sep = ",", stringsAsFactors = TRUE, header = TRUE)
+
+sapply(musicDataSet, class)
+
+musicDataSet[1:9] <- sapply(musicDataSet[1:9],as.numeric)
+sapply(musicDataSet, class)
+
+musicData <- musicDataSet
+head(musicData)
+
+write.csv(musicData, file = "MusicData.csv")
+
+musicDataNorm <- mmnorm(musicData[1:9], minval = 0, maxval = 1)
+head(musicDataNorm)
+
+write.csv(musicDataNorm, file = "MusicDataNorm.csv",row.names = FALSE)
