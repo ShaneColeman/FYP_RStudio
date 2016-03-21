e027856b7f4012a21e8f63c118d8bc3760ad42db
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-03-21 18:02:49 +0000

    Code ammended within TrainTestDataSetsNorm R file, TrainNorm and TestNorm CSV files created and added

--- TrainTestDataSetsNorm.R
+++ TrainTestDataSetsNorm.R
@@ -1,17 +1,24 @@
 #http://stackoverflow.com/questions/17200114/
 #how-to-split-data-into-training-testing-sets-using-sample-function-in-r-program
 
-testDataSetNorm <- read.csv("MusicDataNorm.csv",header = FALSE)
-names(testDataSetNorm) <- NULL
-#50% of whole data set
-sizeDataSet <- floor(0.50 * nrow(testDataSetNorm))
-
 set.seed(11)
+
+testDataSetNorm <- read.csv("MusicDataNorm.csv",header = FALSE)
+head(testDataSetNorm)
+
+#Set the header names to null, represented as NA
+names(testDataSetNorm) <- NULL
+
+#50% of whole data set: MusicDataNorm.csv, stored in variable 'testDataSetNorm'
+sizeDataSet <- floor(0.50 * nrow(testDataSetNorm))
 
 rowsLengthSize <- sample(seq_len(nrow(testDataSetNorm)),size = sizeDataSet)
 
 train <- testDataSetNorm[rowsLengthSize,]
+head(train)
+
 test <- testDataSetNorm[-rowsLengthSize,]
+head(test)
 
 write.csv(train, file = "TrainNorm.csv",row.names = FALSE)
 write.csv(test, file = "TestNorm.csv",row.names = FALSE)
