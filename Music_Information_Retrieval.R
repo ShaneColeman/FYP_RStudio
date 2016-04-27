9fdc61f9da6c10d3dba4be080647e526018334d8
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2016-04-28 00:54:21 +0100

    5 second audio track added, code alteration made in Music Information Retrieval R Script

--- Music_Information_Retrieval.R
+++ Music_Information_Retrieval.R
@@ -2,9 +2,13 @@
 #install.packages("tuneR")
 #install.packages("audio")
 
+#http://samcarcagno.altervista.org/blog/basic-sound-processing-r/?doing_wp_cron=1461188589.7776970863342285156250
+
 library(seewave)
 library(tuneR)
 #library(audio)
+
+#Sampling Frequency: 44100 Hertz or 44.1kHz
 
 ironMaiden <- readWave(filename = "Iron Maiden - Run To The Hills - 5.wav")
 str(ironMaiden)
@@ -15,8 +19,8 @@ str(ironMaidenMono)
 summary(ironMaidenMono)
 
 #Time length of clipped mono track
-numOfSamples <- 230731
-numOfSamples / ironMaidenMono@samp.rate
+numOfSamplesIronMaiden <- 230731
+numOfSamplesIronMaiden / ironMaidenMono@samp.rate
 
 #Confirm that the clipped track is mono and not stereo 
 ironMaidenMonoLeft <- ironMaidenMono@left
@@ -33,10 +37,52 @@ ironMaidenMonoLeft <- ironMaidenMonoLeft / 2 ^ (ironMaidenMono@bit -1)
 summary(ironMaidenMonoLeft)
 
 ironMaidenMonoLength <- length(ironMaidenMonoLeft)
-ironMaidenMonoFFT <- fft(ironMaidenMonoLeft,ironMaidenMonoLength)
+ironMaidenMonoFFT <- fft(ironMaidenMonoLeft)
 
 ironMaidenRMS <- rms(ironMaidenMonoLeft)
-ironMaidenRMS
+ironMaidenRMS #0.2597691
 
-rmsTodB <- 20 * log10(0.2597691)
-rmsTodB
+#http://uk.mathworks.com/matlabcentral/newsreader/view_thread/164978
+rmsTodBIronMaiden <- 10 * log10(0.2597691)
+rmsTodBIronMaiden #-5.854125
+
+################
+
+#Sampling Frequency: 44100 Hertz or 44.1kHz
+
+motorhead <- readWave(filename = "Motorhead - Ace of Spades 5.wav")
+str(motorhead)
+summary(motorhead)
+
+motorheadMono <- mono(motorhead, which = c("both"))
+str(motorheadMono)
+summary(motorheadMono)
+
+#Time length of clipped mono track
+numOfSamplesMotorhead <- 229304
+numOfSamplesMotorhead / motorheadMono@samp.rate
+
+#Confirm that the clipped track is mono and not stereo 
+motorheadMonoLeft <- motorheadMono@left
+summary(motorheadMonoLeft)
+min(motorheadMonoLeft)
+max(motorheadMonoLeft)
+
+motorheadMonoRight <- motorheadMono@right
+summary(motorheadMonoRight)
+min(motorheadMonoRight)
+max(motorheadMonoRight)
+
+motorheadMonoLeft <- motorheadMonoLeft / 2 ^ (motorheadMono@bit -1)
+summary(motorheadMonoLeft)
+
+motorheadMonoLength <- length(motorheadMonoLeft)
+motorheadMonoFFT <- fft(motorheadMonoLeft)
+
+motorheadRMS <- rms(motorheadMonoLeft)
+motorheadRMS #0.2452614
+
+#http://uk.mathworks.com/matlabcentral/newsreader/view_thread/164978
+rmsTodBMotorhead <- 10 * log10(0.2452614)
+rmsTodBMotorhead #-5.854125
+
